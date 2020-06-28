a63b31f451b082ea7c4d163e7115f287beaa6c0c
dmartin5150 <dmartin5150@icloud.com>
2020-06-28 11:59:19 -0500

    Lombak added

--- core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,17 +1,21 @@
 package academy.learnprogramming;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Getter
 @Component
 public class NumberGeneratorImpl implements NumberGenerator {
     // ==fields ==
-    private final Random random = new Random();
+
     private final int maxNumber;
     private final int minNumber;
-
+    @Getter(AccessLevel.NONE)
+    private final Random random = new Random();
     //constructors
 
     @Autowired
@@ -26,13 +30,4 @@ public class NumberGeneratorImpl implements NumberGenerator {
         return random.nextInt(maxNumber - minNumber) + minNumber;
     }
 
-    @Override
-    public int getMaxNumber() {
-        return maxNumber;
-    }
-
-    @Override
-    public int getMinNumber() {
-        return minNumber;
-    }
 }
